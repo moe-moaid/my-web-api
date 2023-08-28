83e0a7bc4570d66c6cb9692930d1ecafdb59046f
moe-moaid <mohammadmoaid@gmail.com>
2023-08-28 16:05:05 +0500

    resolved CORS error

--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace App\Http\Middleware;
+// namespace App\Http\Middleware;
 
-use Closure;
+// use Closure;
 
-class ExampleMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
-    }
-}
+// class ExampleMiddleware
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  \Closure  $next
+//      * @return mixed
+//      */
+//     public function handle($request, Closure $next)
+//     {
+//         return $next($request);
+//     }
+// }
